0db49d88f207c2a6c7c59324676a979e507bec9d
caoxubo <caoxb2004@163.com>
2026-03-07 23:26:41 +0000

    cost submit

--- cost_calculator.py
+++ cost_calculator.py
@@ -1,49 +1,40 @@
 import math
+import numpy as np
 from Hyperparameter import *
+from scipy.spatial.distance import cdist
 
 def calculate_single_station_cost(distances, max_ranges):
     '''
     Calculate the true total cost of constructing a single charging station
     '''
-    valid_distances = []
-    penalty_count = 0  # Record the number of robots requiring fines to be paid
 
-    # Range Check
-    for d, r in zip(distances, max_ranges):
-        if d <= r:
-            valid_distances.append(d)
-        else:
-            penalty_count += 1
-
-    # Capacity Check
-    max_capacity = STATION_CHARGER_LIMIT * CHARGER_ROBOT_LIMIT
-
-    if len(valid_distances) > max_capacity:
-        valid_distances.sort()
-
-        excess_count = len(valid_distances) - max_capacity
-        penalty_count += excess_count
-
-        valid_distances = valid_distances[:max_capacity]
-
-    # Cost Accounting
-    N_served = len(valid_distances)
+    N_served = len(distances)
+    if N_served > CAPACITY_LIMIT:
+        return float('inf')
 
     if N_served == 0:
-        return BUILD_COST + (penalty_count * RESCUE_COST)
+        return 0.0
 
+    # Hardware fixed costs
     num_chargers = math.ceil(N_served / CHARGER_ROBOT_LIMIT)
+    fixed_hardware_cost = BUILD_COST + num_chargers * MAINTAIN_COST
 
-    # Total hardware costs
-    build_and_maintain_cost = BUILD_COST + num_chargers * MAINTAIN_COST
+    # Expected dynamic costs
+    expected_variable_cost = 0.0
 
-    # Flight charging fee
-    flight_cost = sum(valid_distances) * CHARGE_COST
+    for d, r_i in zip(distances, max_ranges):
+        # The probability that the robot requires charging today
+        p_i = math.exp(- (LAMBDA ** 2) * ((r_i - R_MIN) ** 2))
 
-    # Rescue penalty fee
-    penalty_cost = penalty_count * RESCUE_COST
+        # Expected charging fee
+        charge_cost_per_time = CHARGE_COST * R_MAX
+        expected_variable_cost += p_i * charge_cost_per_time
 
-    # Aggregate total cost
-    total_single_station_cost = build_and_maintain_cost + flight_cost + penalty_cost
+        # Expected trailer fee
+        if d > r_i:
+            expected_variable_cost += p_i * RESCUE_COST
 
-    return total_single_station_cost
+    # Total expect cost
+    total_expected_cost = fixed_hardware_cost + expected_variable_cost
+
+    return total_expected_cost
