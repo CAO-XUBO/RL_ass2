a76c117edb1661dbad9e236d36978deccff713da
caoxubo <caoxb2004@163.com>
2026-03-07 18:03:26 +0000

    heuristic submit

--- heuristic.py
+++ heuristic.py
@@ -4,58 +4,11 @@ import pandas as pd
 import os
 from scipy.spatial.distance import cdist
 from Hyperparameter import *
+from cost_calculator import calculate_single_station_cost
 
 def data_load(data_filepath):
     robot_data = pd.read_csv(data_filepath, index_col='index')
     return robot_data
-
-def calculate_single_station_cost(distances, max_ranges):
-    '''
-    Calculate the true total cost of constructing a single charging station
-    '''
-    valid_distances = []
-    penalty_count = 0  # Record the number of robots requiring fines to be paid
-
-    # Range Check
-    for d, r in zip(distances, max_ranges):
-        if d <= r:
-            valid_distances.append(d)
-        else:
-            penalty_count += 1
-
-    # Capacity Check
-    max_capacity = STATION_CHARGER_LIMIT * CHARGER_ROBOT_LIMIT
-
-    if len(valid_distances) > max_capacity:
-        valid_distances.sort()
-
-        excess_count = len(valid_distances) - max_capacity
-        penalty_count += excess_count
-
-        valid_distances = valid_distances[:max_capacity]
-
-    # Cost Accounting
-    N_served = len(valid_distances)
-
-    if N_served == 0:
-        return BUILD_COST + (penalty_count * RESCUE_COST)
-
-    num_chargers = math.ceil(N_served / CHARGER_ROBOT_LIMIT)
-
-    # Total hardware costs
-    build_and_maintain_cost = BUILD_COST + num_chargers * MAINTAIN_COST
-
-    # Flight charging fee
-    flight_cost = sum(valid_distances) * CHARGE_COST
-
-    # Rescue penalty fee
-    penalty_cost = penalty_count * RESCUE_COST
-
-    # Aggregate total cost
-    total_single_station_cost = build_and_maintain_cost + flight_cost + penalty_cost
-
-    return total_single_station_cost
-
 
 def assign_best_available_station(robot_id, station_locations, station_current_counts, robot_data):
     """
