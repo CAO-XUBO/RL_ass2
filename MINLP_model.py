47b0b415827919ec34276289bed811e98128a4ba
Yan4893 <s2819003@ed.ac.uk>
2026-03-28 13:13:40 +0000

    MINLP adjust

--- MINLP_model.py
+++ MINLP_model.py
@@ -16,7 +16,7 @@ def solve_minlp_for_subset(df_subset, name):
     n_robots = len(df_subset)
     I = range(n_robots)
     # Set the number of stations to 8 as per your requirement
-    n_stations =  8
+    n_stations =  3
 
     J = range(n_stations)
     def dist(i, j):
@@ -41,11 +41,12 @@ def solve_minlp_for_subset(df_subset, name):
     prob.addVariable(X_j, Y_j, v_j, z_j, w_ij, h_ij)
 
     # Objective function
-    obj = xp.Sum(BUILD_COST * z_j[j] for j in J) + \
-          xp.Sum(MAINTAIN_COST * v_j[j] for j in J) + \
-          xp.Sum(RESCUE_COST * h_ij[i,j] for i in I for j in J) + \
-          xp.Sum(CHARGE_COST * ((R_MAX - r_i[i]) * w_ij[i,j] + dist(i,j) * (w_ij[i,j] - h_ij[i,j])) for i in I for j in J)
-    
+    build_cost_expr = xp.Sum(BUILD_COST * z_j[j] for j in J)
+    maintain_cost_expr = xp.Sum(MAINTAIN_COST * v_j[j] for j in J)
+    rescue_cost_expr = xp.Sum(RESCUE_COST * h_ij[i, j] for i in I for j in J)
+    charge_cost_expr = xp.Sum(CHARGE_COST * ((R_MAX - r_i[i]) * w_ij[i, j]+ dist(i, j) * (w_ij[i, j] - h_ij[i, j]))for i in I for j in J)
+    # Total objective
+    obj = build_cost_expr + maintain_cost_expr + rescue_cost_expr + charge_cost_expr
     prob.setObjective(obj, sense=xp.minimize)
 
     # Constraints
@@ -61,7 +62,7 @@ def solve_minlp_for_subset(df_subset, name):
     prob.controls.timelimit = 300
     prob.solve()
     
-    return prob, X_j, Y_j, z_j, v_j, x_coords, y_coords
+    return prob, X_j, Y_j, z_j, v_j, w_ij, h_ij, x_coords, y_coords, r_i
 
 # # Results
 import os
@@ -73,7 +74,7 @@ for density in densities:
     df_sub = subsets_df[subsets_df['subset'] == density]
     if not df_sub.empty:
        
-        prob, X_j, Y_j, z_j, v_j, rx, ry = solve_minlp_for_subset(df_sub, density)
+        prob, X_j, Y_j, z_j, v_j, w_ij, h_ij, rx, ry, r_i = solve_minlp_for_subset(df_sub, density)
         
         sol_status = prob.attributes.solstatus
         theoretical_lower_bound = prob.attributes.bestbound
@@ -82,7 +83,24 @@ for density in densities:
             total_cost = prob.attributes.objval
             active_stations_count = 0
             total_chargers = 0
-            
+            I = range(len(rx))
+            J = range(len(X_j))
+            # ----- Cost breakdown -----
+            build_cost_val = sum(BUILD_COST * prob.getSolution(z_j[j])for j in J)
+            maintain_cost_val = sum(MAINTAIN_COST * prob.getSolution(v_j[j])for j in J)
+            rescue_cost_val = sum(RESCUE_COST * prob.getSolution(h_ij[i, j])for i in I for j in J)
+            charge_cost_val = 0.0
+            for i in I:
+                for j in J:
+                    w_val = prob.getSolution(w_ij[i, j])
+                    h_val = prob.getSolution(h_ij[i, j])
+
+                    if w_val > 1e-6:
+                        xj_val = prob.getSolution(X_j[j])
+                        yj_val = prob.getSolution(Y_j[j])
+                        dist_ij = np.sqrt((xj_val - rx[i])**2 + (yj_val - ry[i])**2)
+
+                        charge_cost_val += CHARGE_COST * ((R_MAX - r_i[i]) * w_val+ dist_ij * (w_val - h_val))
             for j in range(len(X_j)):
                 z_val = prob.getSolution(z_j[j])
                 if z_val > 0.5:
@@ -104,6 +122,10 @@ for density in densities:
             summary_results.append({
                 'Scenario': density.capitalize(),
                 'Total_Cost': total_cost,
+                'Build_Cost': build_cost_val,
+                'Maintain_Cost': maintain_cost_val,
+                'Rescue_Cost': rescue_cost_val,
+                'Charge_Cost': charge_cost_val,
                 'Lower_Bound': theoretical_lower_bound,
                 'Stations': active_stations_count,
                 'Avg_Chargers': avg_v
@@ -112,18 +134,39 @@ for density in densities:
             summary_results.append({
                 'Scenario': density.capitalize(),
                 'Total_Cost': None,
+                'Build_Cost': None,
+                'Maintain_Cost': None,
+                'Rescue_Cost': None,
+                'Charge_Cost': None,
                 'Lower_Bound': theoretical_lower_bound,
                 'Stations': None,
                 'Avg_Chargers': None,
                 'Status': str(sol_status)})
-print("\n" + "="*70)
-print(f"{'1(a) FINAL SUMMARY RESULTS':^70}")
-print("="*70)
-print(f"{'DENSITY SCENARIO':<18} | {'TOTAL COST':<15} | {'STATIONS':<10} | {'AVG CHARGERS':<12}")
-print("-" * 70)
+print("\n" + "=" * 140)
+print(f"{'1(a) FINAL SUMMARY RESULTS':^140}")
+print("=" * 140)
+print(
+    f"{'SCENARIO':<12} | {'TOTAL':<12} | {'BUILD':<12} | {'MAINTAIN':<12} | "
+    f"{'RESCUE':<12} | {'CHARGE':<12} | {'STATIONS':<8} | {'AVG CHARGERS':<12}"
+)
+print("-" * 140)
+
+def fmt(x):
+    return f"{x:.2f}" if x is not None else "N/A"
+
 for res in summary_results:
-    print(f"{res['Scenario']:<18} | {res['Total_Cost']:>15} | {res['Stations']:<10} | {res['Avg_Chargers']:<12}")
-print("="*70)
+    print(
+        f"{res['Scenario']:<12} | "
+        f"{fmt(res['Total_Cost']):<12} | "
+        f"{fmt(res['Build_Cost']):<12} | "
+        f"{fmt(res['Maintain_Cost']):<12} | "
+        f"{fmt(res['Rescue_Cost']):<12} | "
+        f"{fmt(res['Charge_Cost']):<12} | "
+        f"{str(res['Stations']):<8} | "
+        f"{fmt(res['Avg_Chargers']):<12}"
+    )
+
+print("=" * 140)
 output_dir = "results/MINLP_results"
 if all_station_data:
     results_df = pd.DataFrame(all_station_data)
